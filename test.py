fcbed76524efb6ec10550a03206e9adf22c69be9
C3NZ <cenz@cenz.io>
2020-03-10 20:02:51 -0700

    [add] the mxnet test I ran to ensure that it is working

---
+++ test.py
@@ -0,0 +1,11 @@
+from mxnet import np, npx
+npx.set_np()
+
+# Anything beyond a 2 dimensional tensor will still use
+# the Frobenius norm to compute the norm.
+A = np.array([[[[1,2,3], [1,2,4]]]])
+B = np.array([[1,2,3], [1,2,4]])
+
+print(np.linalg.norm(B))
+print(np.linalg.norm(A))
+
