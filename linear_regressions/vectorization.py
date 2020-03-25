13fbaba482a0b95f3b6b48a5690e3b740515f0a3
C3NZ <cenz@cenz.io>
2020-03-25 10:52:40 -0700

    [cleanup] the examples and add some doc strings/comments

--- linear_regressions/vectorization.py
+++ linear_regressions/vectorization.py
@@ -32,27 +32,45 @@ class Timer:
         return np.array(self.times).cumsum().tolist()
 
 
-def compute_normal_distribution(z_value, mu, sigma):
-    probability = 1 / math.sqrt(2 * math.pi * sigma ** 2)
-    return probability * np.exp((-0.5 / sigma ** 2) * (z_value - mu) ** 2)
+def compute_normal_distribution(samples, mean, variance):
+    """
+    Compute the probability density of a normal distribution.
+    https://en.wikipedia.org/wiki/Probability_density_function
+    """
+    probability = 1 / math.sqrt(2 * math.pi * variance ** 2)
+    return probability * np.exp((-0.5 / variance ** 2) * (samples - mean) ** 2)
 
 
 def plot_normal_distributions():
+    """
+    Plot normal distributions with different mean (mu) and variance (sigma)
+    values to demonstrate what effects different means and variances has on a
+    normal distribution.
+    """
+    # Create an evenly spaced vector from -7 to 7 with 0.01 as the spacing.
     x = np.arange(-7, 7, 0.01)
 
+    # Different parameters to be used for mean and sigma, respectively.
     parameters = [(0, 1), (0, 2), (3, 1)]
     d2l.plot(
         x,
-        [compute_normal_distribution(x, mu, sigma) for mu, sigma in parameters],
+        [
+            compute_normal_distribution(x, mean, variance)
+            for mean, variance in parameters
+        ],
         xlabel="z",
         ylabel="p(z)",
         figsize=(4.5, 2.5),
-        legend=[f"mean {mu}, var {sigma}" for mu, sigma in parameters],
+        legend=[f"mean {mean}, var {variance}" for mean, variance in parameters],
     )
     d2l.plt.savefig("normal_distributions")
 
 
-def main():
+def compare_speeds_of_computation():
+    """
+    Compare the speeds of vector addition through pythonic code and doing the
+    addition with np.
+    """
     zeros_vector = np.zeros(total_samples_per_vector)
     timer = Timer()
 
@@ -63,10 +81,17 @@ def main():
 
     timer.start()
 
-    new_vector = first_vector + second_vector
+    _ = first_vector + second_vector
     print(f"vector addition with np: {timer.stop():5f} second(s)")
+
+
+def main():
+    """
+    Execute module code.
+    """
+    compare_speeds_of_computation()
+    plot_normal_distributions()
 
 
 if __name__ == "__main__":
     main()
-    plot_normal_distributions()
